0b2bbc887125e80a21cf7f2bbd0128c97bca6812
therealmasterchief <flamethegamer26@gmail.com>
2024-09-16 21:18:08 -0400

    country

---
+++ country.py
@@ -0,0 +1,23 @@
+class india:
+    def capital(self):
+        print("delhi")
+    def language(self):
+        print("most widely spoken is hindi")
+    def type(self):
+        print("developing country")
+class japan:
+    def capital(self):
+        print("tokyo")
+    def language(self):
+        print("japanese")
+    def type(self):
+        print("developed")
+
+x=india()
+x.capital()
+x.language()
+x.type()
+y=japan()
+y.capital()
+y.language()
+y.type()
